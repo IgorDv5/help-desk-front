61895dc2a5e375670736ffb79df71c86c0d64369
IgorDv5 <igoraluno2005@gmail.com>
2024-03-03 20:05:43 -0300

    Componente CHAMADO-CREATE Criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -44,6 +44,7 @@ import { ClienteUpdateComponent } from './components/cliente/cliente-update/clie
 import { ClienteDeleteComponent } from './components/cliente/cliente-delete/cliente-delete.component';
 import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
 import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
+import { ChamadoCreateComponent } from './components/chamado/chamado-create/chamado-create.component';
 
 
 
@@ -65,6 +66,7 @@ import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-
     ClienteDeleteComponent,
     ClienteListComponent,
     ChamadoListComponent,
+    ChamadoCreateComponent,
  
   ],
   imports: [
